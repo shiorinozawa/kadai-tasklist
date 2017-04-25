ff0367f15cec36bb63d0d7f73fa35b1c2d78a9f2
shiorinozawa <lichteen0921@gmail.com>
2017-04-25 00:34:47 +0000

    create usertasks

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      @usertask = current_user.usertasks.build
+      @usertasks = current_user.usertasks.order('created_at DESC').page(params[:page])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
+  resources :usertasks, only: [:create, :destroy]
+  
   #resources :tasks
 end
